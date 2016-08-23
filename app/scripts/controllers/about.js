cc0c57aeb5b4c672cbc7e82ea97f2f8ed4ff6551
Apple <apple@Apples-MacBook-Pro.local>
2016-08-23 07:34:50 +0530

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yokformguiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the yokformguiApp
+ */
+angular.module('yokformguiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name yokformguiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the yokformguiApp
+ */
+angular.module('yokformguiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
